dcaa9e570f8b45bb72df0d4ed77bdd5abbd51eff
Blacker <blacker@aliyun.com>
2017-08-11 14:28:38 +0800

    更新

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -34,49 +34,6 @@ Page({
         this.setMusicMonitor();
     },
 
-    setMusicMonitor: function () {
-        //点击播放图标和总控开关都会触发这个函数
-        var that = this;
-        wx.onBackgroundAudioPlay(function (event) {
-            var pages = getCurrentPages();
-            var currentPage = pages[pages.length - 1];
-            if (currentPage.data.currentPostId === that.data.currentPostId) {
-                // 打开多个post-detail页面后，每个页面不会关闭，只会隐藏。通过页面栈拿到到
-                // 当前页面的postid，只处理当前页面的音乐播放。
-                if (app.globalData.g_currentMusicPostId == that.data.currentPostId) {
-                    // 播放当前页面音乐才改变图标
-                    that.setData({
-                        isPlayingMusic: true
-                    })
-                }
-                // if(app.globalData.g_currentMusicPostId == that.data.currentPostId )
-                // app.globalData.g_currentMusicPostId = that.data.currentPostId;
-            }
-            app.globalData.g_isPlayingMusic = true;
-
-        });
-        wx.onBackgroundAudioPause(function () {
-            var pages = getCurrentPages();
-            var currentPage = pages[pages.length - 1];
-            if (currentPage.data.currentPostId === that.data.currentPostId) {
-                if (app.globalData.g_currentMusicPostId == that.data.currentPostId) {
-                    that.setData({
-                        isPlayingMusic: false
-                    })
-                }
-            }
-            app.globalData.g_isPlayingMusic = false;
-            // app.globalData.g_currentMusicPostId = null;
-        });
-        wx.onBackgroundAudioStop(function () {
-            that.setData({
-                isPlayingMusic: false
-            })
-            app.globalData.g_isPlayingMusic = false;
-            // app.globalData.g_currentMusicPostId = null;
-        });
-    },
-
     onColletionTap: function (event) {
         // this.getPostsCollectedSyc();
         this.getPostsCollectedAsy();
@@ -106,28 +63,6 @@ Page({
         this.showToast(postsCollected, postCollected);
     },
 
-    showModal: function (postsCollected, postCollected) {
-        var that = this;
-        wx.showModal({
-            title: "收藏",
-            content: postCollected ? "收藏该文章？" : "取消收藏该文章？",
-            showCancel: "true",
-            cancelText: "取消",
-            cancelColor: "#333",
-            confirmText: "确认",
-            confirmColor: "#405f80",
-            success: function (res) {
-                if (res.confirm) {
-                    wx.setStorageSync('posts_collected', postsCollected);
-                    // 更新数据绑定变量，从而实现切换图片
-                    that.setData({
-                        collected: postCollected
-                    })
-                }
-            }
-        })
-    },
-
     showToast: function (postsCollected, postCollected) {
         // 更新文章是否的缓存值
         wx.setStorageSync('posts_collected', postsCollected);
@@ -141,28 +76,6 @@ Page({
             icon: "success"
         })
     },
-
-    onShareTap: function (event) {
-        var itemList = [
-            "分享给微信好友",
-            "分享到朋友圈",
-            "分享到QQ",
-            "分享到微博"
-        ];
-        wx.showActionSheet({
-            itemList: itemList,
-            itemColor: "#405f80",
-            success: function (res) {
-                // res.cancel 用户是不是点击了取消按钮
-                // res.tapIndex 数组元素的序号，从0开始
-                wx.showModal({
-                    title: "用户 " + itemList[res.tapIndex],
-                    content: "用户是否取消？" + res.cancel + "现在无法实现分享功能，什么时候能支持呢"
-                })
-            }
-        })
-    },
-
     onMusicTap: function (event) {
         var currentPostId = this.data.currentPostId;
         var postData = postsData.postList[currentPostId];
